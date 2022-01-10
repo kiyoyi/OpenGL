3242b95cec4b40bb38035f83df833672215cfab5
Jiaqi Li <jiaqi.li@arm.com>
2022-01-10 17:40:43 +0800

    vertex color attribute

--- Source.cpp
+++ Source.cpp
@@ -15,19 +15,20 @@ void processInput(GLFWwindow* window)
 
 const char* vertexShaderSource = "#version 330 core\n"
 	"layout (location = 0) in vec3 aPos;\n"
-	"out vec4 vertexColor;"
+	"layout (location = 1) in vec3 aColor;\n"
+	"out vec3 ourColor;"
 	"void main()\n"
 	"{\n"
 	"	gl_Position = vec4(aPos, 1.0);\n"
-	"	vertexColor = vec4(0.5, 0.0, 0.0, 1.0);\n"
+	"	ourColor = aColor;\n"
 	"}\n";
 
 const char* fragmentShaderSource = "#version 330 core\n"
 	"out vec4 FragColor;\n"
-	"uniform vec4 ourColor;"
+	"in vec3 ourColor;"
 	"void main()\n"
 	"{\n"
-	"	FragColor = ourColor;\n"
+	"	FragColor = vec4(ourColor, 1.0);\n"
 	"}\n";
 
 int main()
@@ -104,9 +105,9 @@ int main()
 	// TRIANGLE
 	// set up vertex data and configure vertex attributes
 	float firstVertices[] = {
-		-0.5f,	-0.5f,	0.0f,
-		0.5f,	-0.5f,	0.0f,
-		0.0f,	0.5f,	0.0f
+		-0.5f,	-0.5f,	0.0f,	1.0f, 0.0f, 0.0f, 
+		0.5f,	-0.5f,	0.0f,	0.0f, 1.0f, 0.0f,
+		0.0f,	0.5f,	0.0f,	0.0f, 0.0f, 1.0f
 	};
 
 	float secondVertices[] = {
@@ -136,8 +137,10 @@ int main()
 	glBufferData(GL_ARRAY_BUFFER, sizeof(firstVertices), firstVertices, GL_STATIC_DRAW);
 	//glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
 	//glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_TRUE, 3 * sizeof(float), (void*)0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
 
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 	//glBindVertexArray(0);
